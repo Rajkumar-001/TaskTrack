ef530774d60c4d598ae5751d573e78a232b6ac4a
root <root@Rajkumapandey01>
2024-06-25 23:38:05 +0530

    third commit

---
+++ index.js
@@ -0,0 +1,61 @@
+
+
+function sum(a,b){
+return a+b;
+}
+
+function sub(a,b){
+return a-b;
+
+}
+
+function mul(a,b){
+return a*b;
+}
+
+function anoy(a,b,fn){
+
+let a1=fn(
+function sum(a,b){
+return a+b;
+}
+
+function sub(a,b){
+return a-b;
+
+}
+
+function mul(a,b){
+return a*b;
+}
+
+function anoy(a,b,fn){
+
+let a1=fn(a,b);
+
+return a1*a1;
+
+}
+
+let res=anoy(1,2,sum);
+console.log(res);
+
+
+
+a,b);
+
+return a1*a1;
+
+}
+
+let res=anoy(1,2,sum);
+console.log(res);
+
+
+
+
+
+
+
+
+>
